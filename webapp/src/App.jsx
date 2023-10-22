82d5577eecf8bf6929cc8a36ccb15d7174788953
Leductai98 <leductai9898@gmail.com>
2023-10-22 23:57:25 +0700

    update 22/10/2023

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -6,7 +6,10 @@ import { useNavigate } from 'react-router-dom'
 function App() {
   const navigate=useNavigate()
   useEffect(()=>{
-    navigate("/receipt")
+    if(location.pathname==='/'){
+      navigate("/receipt")
+    }
+   
   },[])
   return (
    <>
